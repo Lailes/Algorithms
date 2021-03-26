99bb2a9da8f42aa83148da16c81dcc07415a41db
Auriel <Amadera@protonmail.com>
2021-03-27 00:20:01 +0300

    Add Merge Sort

--- Algorithms.Tests/MergeTests.cs
+++ Algorithms.Tests/MergeTests.cs
@@ -1,7 +1,42 @@
-﻿namespace Algorithms.Tests
+﻿using Sorting.Algorithms;
+using Xunit;
+
+namespace Algorithms.Tests
 {
     public class MergeTests
     {
+
+        [Theory]
+        [InlineData(new []{1,2,3,4},  new []{1,2}, new []{3,4})]
+        [InlineData(new []{1,2,3},  new []{1}, new []{2,3})]
+        [InlineData(new []{1,2},  new []{1}, new []{2})]
+        [InlineData(new []{1},  new []{1}, null)]
+        public void TestSplit(int[] array, int[] arr1, int[] arr2) {
+
+            var alg = new MergeSort<int>();
+
+            var (a, b) = alg.Split(array);
+            Assert.Equal(arr1, a);
+            Assert.Equal(arr2, b);
+        }
+
+
+        [Theory]
+        [InlineData(new []{1,4,7}, new []{2,5,8}, new []{1,2,4,5,7,8})]
+        [InlineData(new []{1,4,7}, null, new []{1,4,7})]
+        [InlineData(null, new []{2,5,8}, new []{2,5,8})]
+        [InlineData(new []{1,5,8}, new []{2,5,8}, new []{1,2,5,5,8,8})]
+        [InlineData(new []{1,4}, new []{2,5,8}, new []{1,2,4,5,8})]
+        [InlineData(new []{1}, new []{2,5,8}, new []{1,2,5,8})]
+        public void TestMerge(int[] arr1, int[] arr2, int[] array) {
+            var alg = new MergeSort<int>();
+            var res = alg.Merge(arr1, arr2);
+            Assert.Equal(array, res);
+        }
         
+        [Theory]
+        [InlineData(new[] {3, 1, 8, 5, 6, 1, 2, 3, 0, 8, 4, 12, 11, 14, 17, 20, 19, 18},
+            new[] {0, 1, 1, 2, 3, 3, 4, 5, 6, 8, 8, 11, 12, 14, 17, 18, 19, 20})]
+        public void TestMergeSort(int[] input, int[] ideal) => Assert.Equal(ideal, new MergeSort<int>().Sort(input));
     }
 }
--- Algorithms/Algorithms/MergeSort.cs
+++ Algorithms/Algorithms/MergeSort.cs
@@ -5,7 +5,7 @@ namespace Sorting.Algorithms
     public class MergeSort<T> : ISorter<T> where T : IComparable<T>
     {
 
-        public (int[], int[]) Split(int[] arr)
+        public (T[], T[]) Split(T[] arr)
         {
             if (arr.Length == 1)
             {
@@ -13,19 +13,84 @@ namespace Sorting.Algorithms
             }
 
             var length = arr.Length;
+            var half = length / 2;
             int i;
-            for (i = 0; i < length / 2; i++)
+            var arr1 = new T[half];
+            var arr2 = new T[arr.Length - half];
+            for (i = 0; i < half; i++)
             {
-                
+                arr1[i] = arr[i];
             }
 
-            throw new NotImplementedException();
+            var a = 0;
+            for (; i < arr.Length; i++) {
+                arr2[a++] = arr[i];
+            }
+
+            return (arr1, arr2);
+        }
+
+        public T[] Merge(T[] arr1, T[] arr2) {
+            if (arr1 == null && arr2 == null) {
+                return null;
+            }
+
+            if (arr1 == null) {
+                return arr2;
+            }
+
+            if (arr2 == null) {
+                return arr1;
+            }
+
+            var result = new T[arr1.Length + arr2.Length];
+
+            var index1 = 0;
+            var index2 = 0;
+            for (var i = 0; i < result.Length; i++) {
+                if (index1 == arr1.Length && index2 == arr2.Length) {
+                    break;
+                }
+
+                if (index1 == arr1.Length) {
+                    result[i] = arr2[index2++];
+                    continue;
+                }
+                if (index2 == arr2.Length) {
+                    result[i] = arr1[index1++];
+                    continue;
+                }
+
+                var a = arr1[index1];
+                var b = arr2[index2];
+
+                switch (a.CompareTo(b)) {
+                    case 0:
+                        result[i] = a;
+                        index1++;
+                        break;
+                    case < 0:
+                        result[i] = a;
+                        index1++;
+                        break;
+                    default:
+                        result[i] = b;
+                        index2++;
+                        break;
+                }
+            }
+
+            return result;
         }
         
         
-        public T[] Sort(T[] array)
-        {
-            throw new NotImplementedException();
+        public T[] Sort(T[] array) {
+            if (array == null || array.Length == 1) {
+                return array;
+            }
+            var (a, b) = Split(array);
+            var result = Merge(Sort(a), Sort(b));
+            return result;
         }
     }
 }
